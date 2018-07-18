02ca2be6fad73835db173dc8d5cfbd325d34c9bc
ryanwevans <ryanwevans@icloud.com>
2018-07-17 18:06:47 -0700

    add nested resources and helpers for creating and editing posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :authors, only: [:show, :index] do
-    resources :posts, only: [:show, :index]
+    resources :posts, only: [:show, :index, :new, :edit] #the :new gives us access to authors/:id/posts/new and a new_author_post_path helper
   end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
